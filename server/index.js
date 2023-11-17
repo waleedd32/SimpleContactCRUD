762f900737caf46e6a695e9b78737782eb48c2b6
waleedd32 <valid0@hotmail.com>
2023-11-17 16:24:34 +0200

    Added POST route for create and integrated axios on frontend

--- server/index.js
+++ server/index.js
@@ -8,4 +8,8 @@ app.get("/", async (req, res) => {
   res.json({ message: "Server is running crud" });
 });
 
+app.post("/create", async (req, res) => {
+  console.log("successfully sent to server", req.body);
+});
+
 app.listen(PORT, () => console.log("Server is runningon port:", PORT));
