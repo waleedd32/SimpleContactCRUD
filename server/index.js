5a70fb49fe8d1a2d45b751e8ace09410324d0578
waleedd32 <valid0@hotmail.com>
2024-02-07 19:19:40 +0200

    added url of front end to backend

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
 
-//schema
+//schema for user
 const UserSchema = mongoose.Schema(
   {
     name: String,
